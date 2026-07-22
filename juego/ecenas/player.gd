2bdcec842eb5d72804cfb8a7805ab0058977b80d
Alejandro28jpo <alejandrojpo28@gmail.com>
2026-07-22 17:20:13 -0400

    a

--- juego/ecenas/player.gd
+++ juego/ecenas/player.gd
@@ -1,22 +1,13 @@
 extends CharacterBody2D
 
-var Destination = Vector2()
-var Distance = Vector2()
-var Velocity = Vector2()
+var direccion : Vector2
 
-@export var speed = 250.0
-
-func _ready() -> void:
-	Destination = position
-
-func _physics_process(delta: float) -> void:
-	if (position != Destination):
-		Distance = Vector2(Destination - position)
-		Velocity.x = Distance.normalized().x * speed
-		Velocity.y = Distance.normalized().y * 0
-		velocity = Velocity
-		move_and_slide()
-	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("click"):
-		Destination = get_global_mouse_position()
+		direccion = get_global_mouse_position()
+
+func _physics_process(delta: float) -> void:
+	var vel = (direccion - self.global_position)
+	vel = vel.clamp(Vector2(-100,-100), Vector2(100,100))
+	velocity = vel
+	move_and_slide()
