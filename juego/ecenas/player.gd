e2f6a70e636d4826487f292564042023c8f5247f
Alejandro28jpo <alejandrojpo28@gmail.com>
2026-07-22 17:05:03 -0400

    a2

--- juego/ecenas/player.gd
+++ juego/ecenas/player.gd
@@ -1,25 +1,22 @@
 extends CharacterBody2D
 
+var Destination = Vector2()
+var Distance = Vector2()
+var Velocity = Vector2()
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 250.0
 
+func _ready() -> void:
+	Destination = position
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	if (position != Destination):
+		Distance = Vector2(Destination - position)
+		Velocity.x = Distance.normalized().x * speed
+		Velocity.y = Distance.normalized().y * 0
+		velocity = Velocity
+		move_and_slide()
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("click"):
+		Destination = get_global_mouse_position()
