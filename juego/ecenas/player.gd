1478b3fd68a53d59073ee613ad53c438c5ccbe51
ma.gutierrezk <martin.kuruzg@gmail.com>
2026-07-23 02:43:40 -0400

    path_finding_fix
    Arreglado el error del pathfinding

--- juego/ecenas/player.gd
+++ juego/ecenas/player.gd
@@ -9,12 +9,15 @@ func _ready() -> void:
 	agente.target_position = direccion
 
 func _input(event: InputEvent) -> void:
+	
 	if event.is_action_pressed("click"):
 		direccion = get_global_mouse_position()
-
+		agente.set_pathfinding_algorithm(0)
+		agente.target_position = direccion
 func _physics_process(delta: float) -> void:
 	if agente.is_target_reached():
 		velocity = Vector2.ZERO
 	else:
 		var dir:= to_local(agente.get_next_path_position()).normalized()
-		velocity = dir * speed
+		velocity = velocity.lerp(dir * speed, delta)
+	move_and_slide()
