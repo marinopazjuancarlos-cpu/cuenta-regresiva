89249df1e6254be8d0cbed9ec9ecdf0361c19182
Shivrik <escuderobeltranfranciscojavier@gmail.com>
2026-07-26 11:39:36 +0200

    ...

--- juego/ecenas/sala_cables.gd
+++ juego/ecenas/sala_cables.gd
@@ -6,7 +6,7 @@ const RUTA_OFICINA = "uid://cfd61lgqbjf37"
 func _ready() -> void:
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 		ControladorTransiciones.ir_a_escena(RUTA_OFICINA)
