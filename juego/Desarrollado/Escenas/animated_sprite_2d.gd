3891c5fe2d9c1ceb6e4eb7ddf3e8a6d1867820d8
Shivrik <escuderobeltranfranciscojavier@gmail.com>
2026-07-27 16:42:47 +0200

    radio cigarro

--- juego/Desarrollado/Escenas/animated_sprite_2d.gd
+++ juego/Desarrollado/Escenas/animated_sprite_2d.gd
@@ -1,25 +1,20 @@
 extends AnimatedSprite2D
-var encendido : int
-@onready var radio = [preload("uid://tvord3t7e8m8"),preload("uid://bt6luyhhjiecw")]
+var encendido = -1
+@onready var radio = [preload("uid://bt6luyhhjiecw"),preload("uid://tvord3t7e8m8")]
 func _ready() -> void:
 	pass
-	
-
-# Called when the node enters the scene tree for the first time.
-
-
-
 
 func _on_radio_2_pressed() -> void:
-	$".".play("default")
+	encendido += 1
 	
+	if encendido == 0:
+		$".".play("default")
+		ControladorAudio.reproducir_musica(radio[encendido])
 	if encendido == 1:
-		
-		ControladorAudio.reproducir_musica(preload("uid://pby5qpqwos21"))
+		$".".play("default")
+		ControladorAudio.reproducir_musica(radio[encendido])
+	if encendido == 2:
+		$".".stop()
+		ControladorAudio.detener_musica()
+	if encendido >= 3:
 		encendido = 0
-		ControladorAudio.reproducir_musica(radio[encendido])
-	else:
-		ControladorAudio.reproducir_musica(preload("uid://pby5qpqwos21"))
-		encendido = 1
-		ControladorAudio.reproducir_musica(radio[encendido])
-	#ControladorAudio.reproducir_musica(preload("res://OST/RADIO_CASSETE_MÚSICA_PUNK.ogg"))
